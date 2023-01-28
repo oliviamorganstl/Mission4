55617e950acd49acc670fb8d9189c35eabe35bfd
oliviamorganstl <95643022+oliviamorganstl@users.noreply.github.com>
2023-01-27 22:52:50 -0700

    Add project files.

---
+++ Mission4/wwwroot/js/calculate.js
@@ -0,0 +1,26 @@
+﻿$("#btnsend").click(function () { //This section calculates the weights for different grade categories
+    var grade = (parseInt($("#assign").val()) * 0.5);
+    grade += (parseInt($("#proj").val()) * 0.1);
+    grade += (parseInt($("#quiz").val()) * 0.1);
+    grade += (parseInt($("#mid").val()) * 0.1);
+    grade += (parseInt($("#fin").val()) * 0.1);
+    grade += (parseInt($("#intex").val()) * 0.1);
+    var letterGrade;
+    $('#formform').fadeOut("slow");
+
+    //Loop through to see what the letter grade is
+    if (grade >= 90) {
+        letterGrade = 'A';
+    }
+    else if (grade >= 80) {
+        letterGrade = 'B';
+    }
+    else if (grade >= 70) {
+        letterGrade = 'C';
+    }
+    else if (grade >= 60) {
+        letterGrade = 'D';
+    }
+
+    document.getElementById("announce").innerHTML = ("Your class grade is a " + grade.toString() + "%. Your letter grade is " + letterGrade + ".");
+})
